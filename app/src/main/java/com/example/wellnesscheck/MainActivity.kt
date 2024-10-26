dbdfd298011884f08eb6a952bc9e16f7405412d9
kushalk173-sc <kushhhh25@gmail.com>
2024-10-26 18:07:50 -0400

    Home screen - started

--- app/src/main/java/com/example/wellnesscheck/MainActivity.kt
+++ app/src/main/java/com/example/wellnesscheck/MainActivity.kt
@@ -1,10 +1,10 @@
+// MainActivity.kt
 package com.example.wellnesscheck
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.wellnesscheck.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -22,14 +22,16 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        // Set up AppBarConfiguration with IDs that match those in the menu
+        // Set up AppBarConfiguration if you need it with the menu IDs
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        // Removing this line if you don't have an ActionBar set
+        // setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
 }
